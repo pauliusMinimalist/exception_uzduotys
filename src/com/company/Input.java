7c5572b148883a15857f5458e6d09731423becf3
minimalistPaulius <remeikucis1@gmail.com>
2020-07-18 22:21:47 +0200

    finished 2nd task

---
+++ src/com/company/Input.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Input {
+    public int iveskiteSkaiciu() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Įveskite Skaičių: ");
+        return in.nextInt();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.util.InputMismatchException;
+
 public class Main {
 
     public static void main(String[] args) {
         A a = new A();
         a.klaida();
+        System.out.println("/////////////////////////////////");
+
+        Input input = new Input();
+        int skaicius = 0;
+        while (skaicius == 0) {
+            try {
+                skaicius = input.iveskiteSkaiciu();
+                System.out.println("Jūsų skaičius: " + skaicius);
+            } catch (InputMismatchException e) {
+                System.out.println("Netinkami duomenys!");
+            }
+        }
     }
 }
